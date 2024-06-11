3409a2b0e5e2f68b79d7abd749e0c14c79af8d12
ravi2krishna <ravi2krishna@gmail.com>
2024-06-11 08:37:15 +0530

    fix conflict address

--- vpc.tf
+++ vpc.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "ibm_web_sn" {
 # Create Subnet for Database Servers
 resource "aws_subnet" "ibm_db_sn" {
   vpc_id     = aws_vpc.ibm_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-west-2b"
   map_public_ip_on_launch = "false"
 
