e8620d37d814d067845c2eec8b02e639e193fde0
ravi2krishna <ravi2krishna@gmail.com>
2024-06-14 08:17:50 +0530

    fix issue

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ibm_web_server" {
   instance_type = "t2.micro"
   key_name = "730"
   subnet_id = aws_subnet.ibm_web_sn.id
-  vpc_security_group_ids = aws_security_group.ibm_web_sg.id
+  vpc_security_group_ids = [aws_security_group.ibm_web_sg.id]
   user_data = file("app.sh")
 
   tags = {
